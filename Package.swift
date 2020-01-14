34dc5a4a1c82c58b5c245e1a28be2202206d36f5
fandong <admin@fandong.me>
2020-01-14 11:29:46 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "FDNetwork",
     
-//    platforms: [
-//            .iOS(.v13),
-//    ],
+    platforms: [
+            .iOS(.v13),
+    ],
     
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -20,13 +20,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FDNetwork",
-            dependencies: []),
+            dependencies: [
+            "Alamofire",
+        ]),
         .testTarget(
             name: "FDNetworkTests",
             dependencies: ["FDNetwork"]),
